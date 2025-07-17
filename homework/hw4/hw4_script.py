8c373ce6dc5145199eae0a38ef774758b0426c0c
Tristan <86117774+knockout101@users.noreply.github.com>
2025-07-17 09:41:56 -0500

    Continuation of the work on hw4 in python

--- homework/hw4/hw4_script.py
+++ homework/hw4/hw4_script.py
@@ -25,9 +25,12 @@ class ENZ:
 class ECEF:
     rCGEarth = 0
     vCGEarth = 0
+    EARTH_ROTATION_RATE = np.array([0, 0, fd_const.EARTH_ROTATION_RATE])  # Earth rotation velocity in ECEF frame
 
     def __str__(self):
-        return f"ECEF.rCGEarth = \n{self.rCGEarth}"
+        return f"""ECEF.rCGEarth = \n{self.rCGEarth}\n
+ECEF.vCGEarth = \n{self.vCGEarth}\n
+ECEF.earth_rotation_velocity = \n{self.EARTH_ROTATION_RATE}"""
 
     @staticmethod
     def coordinates_to_ECEF(lat_rad, lon_rad, elevation) -> np.array:
@@ -37,13 +40,50 @@ class ECEF:
         y = (fd_const.EARTH_RADIUS + elevation) * np.cos(lat_rad) * np.sin(lon_rad)
         z = (fd_const.EARTH_RADIUS + elevation) * np.sin(lat_rad)
         
-        return np.transpose(np.atleast_2d([x, y, z]))
+        return np.atleast_2d([
+            [x], 
+            [y], 
+            [z]
+            ])
+    
+    @staticmethod
+    def ECEF_to_NED(ECEF_velocity, lat_rad, lon_rad) -> np.array:
+        """Convert ECEF velocity to NED."""
+        return np.matmul(ECEF.tNED(lat_rad=lat_rad, lon_rad=lon_rad), ECEF_velocity)
+    @staticmethod
+    def tNED(lat_rad, lon_rad):
+        """Transformation matrix from ECEF to NED."""
+        # Transformation matrix from ECEF to NED
+        return np.array([
+            [-np.sin(lat_rad) * np.cos(lon_rad), -np.sin(lat_rad) * np.sin(lon_rad), np.cos(lat_rad)],
+            [-np.sin(lon_rad), np.cos(lon_rad), 0],
+            [-np.cos(lat_rad) * np.cos(lon_rad), -np.cos(lat_rad) * np.sin(lon_rad), -np.sin(lat_rad)]
+        ])
+    
+    
 
 class ECI:
     pass
 
 class NED:
-    pass
+    vCGCG = 0
+
+    def __str__(self):
+        return f"NED.vCGCG = \n{self.vCGCG}"
+
+    @staticmethod
+    def NED_to_ECEF(NED_velocity, lat_rad, lon_rad) -> np.array:
+        """Convert NED velocity to ECEF."""
+        return np.matmul(NED.tECEF(lat_rad=lat_rad, lon_rad=lon_rad), NED_velocity)
+    @staticmethod
+    def tECEF(lat_rad, lon_rad):
+        """Transformation matrix from NED to ECEF."""
+        # Transformation matrix from NED to ECEF
+        return np.array([
+            [-np.sin(lat_rad) * np.cos(lon_rad), -np.sin(lon_rad), -np.cos(lat_rad) * np.cos(lon_rad)],
+            [-np.sin(lat_rad) * np.sin(lon_rad), np.cos(lon_rad), -np.cos(lat_rad) * np.sin(lon_rad)],
+            [np.cos(lat_rad), 0, -np.sin(lat_rad)]
+        ])
     
 
 def main():
@@ -65,9 +105,15 @@ def main():
     lon_rad = np.radians(lon)
 
     ecef = ECEF()
+    ned = NED()
 
     ecef.rCGEarth = ECEF.coordinates_to_ECEF(lat_rad, lon_rad, elevation)
-    ecef.vCGEarth = 
+    ned.vCGCG = np.atleast_2d(
+        [[0], 
+         [0], 
+         [0]]
+    )  # Assuming no velocity for this example
+    ecef.vCGEarth = NED.NED_to_ECEF(ned.vCGCG, lat_rad, lon_rad)
 
     print(ecef)
 
